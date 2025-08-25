fd594f6f2ae21a24e08af259546ce817678138ca
99slayer <21979862+99slayer@users.noreply.github.com>
2025-08-25 17:23:01 -0600

    feat: Add message loader logic and styles

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,33 +1,70 @@
-import type { JSX } from "react";
+import React, {
+	useEffect,
+	useRef,
+	useState,
+	type JSX
+} from "react";
 import messageArr from "../messageData";
 import type { MessageInterface } from "../types";
 
 export default function Chat() {
-	function createMessages(arr: MessageInterface[]) {
-		const elementArr: JSX.Element[] = [];
+	const [messages, setMessages] = useState<JSX.Element[]>([]);
+	const [responding, setResponding] = useState<boolean>(true);
 
-		for (const x of arr) {
-			elementArr.push(<Message key={Math.random()} content={x} />);
+	useEffect(() => {
+		if (messages.length === messageArr.length) {
+			setResponding(false);
+			return;
 		}
 
-		return elementArr;
-	}
+		// message timer
+		const interval = setInterval(() => {
+			setResponding(false);
+			setMessages([
+				...messages,
+				<Message content={messageArr[messages.length]} />
+			]);
+		}, 3000);
+
+		// loader timer
+		const timeout = setTimeout(() => {
+			setResponding(true);
+		}, 1500);
+
+		return () => {
+			clearInterval(interval);
+			clearTimeout(timeout);
+		};
+	}, [messages]);
 
 	return (
 		<div className="max-w-[1000px] w-[100%] mx-auto">
 			<div className="py-20 px-10 flex flex-col gap-6">
-				{createMessages(messageArr)}
+				{messages}
+				{responding ? <ResponseIndicator /> : <></>}
 			</div>
 		</div>
 	);
 }
 
 function Message({ content }: { content: MessageInterface }) {
+	const ref: React.RefObject<HTMLDivElement | null> = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const x = setTimeout(() => {
+			ref.current!.classList.remove('hidden-user-1');
+			ref.current!.classList.remove('hidden-user-2');
+		}, 100);
+
+		return () => clearTimeout(x);
+	}, []);
+
 	return (
 		<div
+			ref={ref}
 			className={`message ${content.userId === Number(import.meta.env.VITE_USER_ID)
-				? "message-user"
-				: "message-nonuser"
+				? "message-user-1 hidden-user-1"
+				: "message-user-2 hidden-user-2"
 				}`}
 		>
 			<header className="flex justify-between">
@@ -35,6 +72,16 @@ function Message({ content }: { content: MessageInterface }) {
 				<h3>{content.timestamp}</h3>
 			</header>
 			<p>{content.text}</p>
+		</div>
+	);
+}
+
+function ResponseIndicator() {
+	return (
+		<div className="justify-self-center self-center p-2 flex gap-1 rounded-full bg-primary">
+			<div className="loader loader-1"></div>
+			<div className="loader loader-2"></div>
+			<div className="loader loader-3"></div>
 		</div>
 	);
 }

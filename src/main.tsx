4194b16cab323b278a361b3018afe3ce96e564f8
99slayer <21979862+99slayer@users.noreply.github.com>
2025-09-19 17:40:09 -0600

    build: deployment prep

--- src/main.tsx
+++ src/main.tsx
@@ -7,30 +7,33 @@ import Chat from './components/Chat.tsx';
 import Column from './components/Column.tsx';
 import Carousel from './components/Carousel.tsx';
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		Component: App,
-		children: [
-			{
-				index: true,
-				element: <Navigate to='chat' replace />
-			},
-			{
-				path: "chat",
-				Component: Chat
-			},
-			{
-				path: 'column',
-				Component: Column
-			},
-			{
-				path: 'carousel',
-				Component: Carousel
-			}
-		]
-	}
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			Component: App,
+			children: [
+				{
+					index: true,
+					element: <Navigate to='chat' replace />
+				},
+				{
+					path: "chat",
+					Component: Chat
+				},
+				{
+					path: 'column',
+					Component: Column
+				},
+				{
+					path: 'carousel',
+					Component: Carousel
+				}
+			]
+		}
+	],
+	{ basename: '/frontend-collection/' }
+);
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
